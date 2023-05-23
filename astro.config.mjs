20e03f3b6f314e07b9ccf747a67abdb2b8b69bfc
Dwane Hemmings <hey@dwane.io>
2023-05-23 11:57:26 -0400

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify/functions";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+  integrations: [svelte()]
+});
